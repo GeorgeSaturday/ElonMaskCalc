e611f68437b4aa0edfc20205f7b56a96d25962ff
GeorgeSaturday <subbotin.97@inbox.ru>
2018-11-01 14:49:21 +0300

    WinCalc

---
+++ EM.Calc.ConsoleApp/EM.Calc.WinCalc/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace EM.Calc.WinCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbOperation = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.tbinput = new System.Windows.Forms.TextBox();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // cbOperation
+            // 
+            this.cbOperation.FormattingEnabled = true;
+            this.cbOperation.Location = new System.Drawing.Point(33, 26);
+            this.cbOperation.Name = "cbOperation";
+            this.cbOperation.Size = new System.Drawing.Size(406, 21);
+            this.cbOperation.TabIndex = 0;
+            this.cbOperation.SelectedIndexChanged += new System.EventHandler(this.cbOperation_SelectedIndexChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(336, 231);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 24);
+            this.lblResult.TabIndex = 1;
+            // 
+            // tbinput
+            // 
+            this.tbinput.Location = new System.Drawing.Point(33, 141);
+            this.tbinput.Name = "tbinput";
+            this.tbinput.Size = new System.Drawing.Size(406, 20);
+            this.tbinput.TabIndex = 2;
+            this.tbinput.TextChanged += new System.EventHandler(this.tbinput_TextChanged);
+            this.tbinput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbinput_KeyPress);
+            // 
+            // btnExec
+            // 
+            this.btnExec.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExec.Location = new System.Drawing.Point(33, 191);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(240, 140);
+            this.btnExec.TabIndex = 3;
+            this.btnExec.Text = "Вычислить";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(146, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(252, 77);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(364, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введите операнды через пробел:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Выберите операцию из списка:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(336, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnExec);
+            this.Controls.Add(this.tbinput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cbOperation);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbOperation;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox tbinput;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ EM.Calc.ConsoleApp/EM.Calc.WinCalc/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EM.Calc.WinCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Core.Calc Calc { get; set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Calc = new Core.Calc();
+            
+                string[] operations = Calc.Operations
+                .Select(o => o.Name)
+                .ToArray();
+
+            cbOperation.Items.AddRange(operations);
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var values = tbinput.Text
+                .Split(' ').Select(Convert.ToDouble)
+                .ToArray();
+                var operation = cbOperation.Text;
+                var result = Calc.Execute(operation, values);
+                lblResult.Text = $"{result}";
+            }
+            catch
+            {
+                MessageBox.Show("Неккоректный ввод");
+            }
+
+
+        }
+
+        private void tbinput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbinput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8 && number != 32 && number != 44 ) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void cbOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var operation = Calc.Operations
+                .Where(o => o.Name == cbOperation.Text)
+                .FirstOrDefault();
+
+            var ope = (IExtOperation)operation;
+
+            toolTip1.SetToolTip(cbOperation, ope.Description);
+        }
+    }
+}
